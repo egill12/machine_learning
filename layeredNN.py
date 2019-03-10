703291f44e9f5a766aeee090acec9b1d9c983479
e gill <egill12@dcs.bbk.ac.uk.com>
2019-03-10 19:29:36 +0000

    adding simple NN calculations

---
+++ layeredNN.py
@@ -0,0 +1,5 @@
+'''
+from https://iamtrask.github.io/2015/07/12/basic-python-network/
+trying to understand the functionality of a NN
+'''
+import numpy as np
---
+++ simpleNN.py
@@ -0,0 +1,32 @@
+'''
+simple NN as an example of how it works
+from --> https://iamtrask.github.io/2015/07/12/basic-python-network/
+'''
+
+import pandas as pd
+import numpy as np
+
+def main():
+    '''
+
+    :return:
+    '''
+    X = np.array([[0, 0, 1], [0, 1, 1], [1, 0, 1], [1, 1, 1]])
+    y = np.array([[0, 1, 1, 0]]).T
+    syn0 = 2 * np.random.random((3, 4)) - 1
+    syn1 = 2 * np.random.random((4, 1)) - 1
+
+    for j in range(0,6000):
+        l1 = 1 / (1 + np.exp(-(np.dot(X, syn0))))
+        l2 = 1 / (1 + np.exp(-(np.dot(l1, syn1))))
+        l2_delta = (y - l2) * (l2 * (1 - l2))
+        l1_delta = l2_delta.dot(syn1.T) * (l1 * (1 - l1))
+        syn1 += l1.T.dot(l2_delta)
+        syn0 += X.T.dot(l1_delta)
+
+    final = np.dot(X,syn0)
+    Y_hat = np.dot(final,syn1)
+    print(Y_hat)
+
+if __name__ == "__main__":
+    main()
